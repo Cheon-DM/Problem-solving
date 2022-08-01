e010d09e856819944306f738a0c4a88fdf004e2b
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-08-02 01:23:02 +0900

    Create (25083)새싹.cpp

---
+++ Bronze/(25083)새싹.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    printf("         ,r\'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r\'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+}
+
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
