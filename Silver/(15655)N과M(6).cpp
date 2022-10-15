0e791e92071fc75eb4074a69a831bcd10a0d4528
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-10-15 22:20:15 +0900

    Create (15655)N과M(6).cpp

---
+++ Silver/(15655)N과M(6).cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int num[9];
+int chk[9] = {0, };
+int result[9] = {0, };
+
+void sol(int level, int start) {
+    if (level == m) {
+        for (int i = 0; i < m; i++) {
+            cout << result[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = start; i < n; i++) {
+        if (!chk[i]) {
+            chk[i] = 1;
+            result[level] = num[i];
+            sol(level + 1, i);
+            chk[i] = 0;
+        }
+    }
+}
+
+int main() {
+    //input
+    cin >> n >> m;
+    for (int i = 0; i < n; i++){
+        cin >> num[i];
+    }
+
+    sort(num, num+n);
+    
+    //sol
+    sol(0, 0);
+
+    return 0;
+}
