ed77759cb4c95b95f07fe8ce3ca57b9d389bf9a4
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-17 01:59:47 +0900

    Create (257)Binary_Tree_Paths.cpp

---
+++ LeetCode/(257)Binary_Tree_Paths.cpp
@@ -0,0 +1,36 @@
+/**
+ * Leetcode - 257
+ * string, backtracking, tree, dfs, binary tree
+*/
+
+class Solution {
+public:
+    vector<string> answer;
+
+    void dfs(TreeNode* node, string current){
+        string val = to_string(node->val);
+
+        if (node->left == NULL && node->right == NULL){
+            answer.push_back(current + val);
+            return;
+        }
+
+        if (node->left != NULL){
+            dfs(node->left, current + val + "->");
+        }
+
+        if (node->right != NULL){
+            dfs(node->right, current + val + "->");
+        }
+
+        return;
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+        if (root != NULL){
+            dfs(root, "");
+        }
+
+        return answer;
+    }
+};
