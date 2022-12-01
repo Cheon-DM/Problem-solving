b9b0fd7fc743b9502e879f3fca98b1d5b82e37ce
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-12-01 18:21:24 +0900

    Create (1207)Unique_Number_of_Occurrences.py

---
+++ LeetCode/(1207)Unique_Number_of_Occurrences.py
@@ -0,0 +1,10 @@
+'''
+' Leetcode - 1207
+' array, hash table
+'''
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        arr_len = len(set(arr))
+        feq_len = len(set(Counter(arr).values()))
+        return arr_len == feq_len
