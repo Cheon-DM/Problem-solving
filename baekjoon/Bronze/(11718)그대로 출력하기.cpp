bdae06788aa3b403e4f00f6b3260d5326de7973b
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-05-09 22:31:07 +0900

    Create (11718)그대로 출력하기.cpp

---
+++ baekjoon/Bronze/(11718)그대로 출력하기.cpp
@@ -0,0 +1,22 @@
+/**
+ * baekjoon - 11718
+ * string
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string str;
+    while (getline(cin, str))
+    {
+        cout << str << '\n';
+    }
+
+    return 0;
+}
