778e92e033fbb3d6862b5624e989f71d46e16637
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-06-11 22:54:47 +0900

    Create (4101)크냐.py

---
+++ baekjoon/Bronze/(4101)크냐.py
@@ -0,0 +1,13 @@
+# baekjoon - 4101
+# implementation
+
+while True:
+    a, b = map(int, input().split())
+    
+    if a == 0 and b == 0:
+        break;
+    
+    if a > b:
+        print("Yes")
+    else:
+        print("No")
